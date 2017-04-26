5a060d8b723c5cd364b410de572ae08635aebbeb
Lev Perlman <lev.perlman@productmadness.com>
2017-04-26 14:48:52 +0100

    aaa

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour {
+
+	private int count;
+	public Text CountText;
+	public Text WinText;
+	public float Speed;
+	private Rigidbody rb;
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+		WinText.text = string.Empty;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		var moveHorizontal = Input.GetAxis ("Horizontal");
+		var moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * Speed);
+	} 
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag("pickup")) {
+			count++;
+			SetCountText ();
+			Destroy (other.gameObject);	
+		}
+
+	}
+
+	private void SetCountText(){
+		CountText.text = "Count: " + count.ToString ();
+		if (count >= 5) {
+			WinText.text = "You win";
+		}
+	}
+} 
---
+++ Assets/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+		var rotateVector = new Vector3 (15, 30, 45);
+		transform.Rotate (rotateVector * Time.deltaTime);
+	}
+}
